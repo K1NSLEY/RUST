7e5817406f49e902deba56a417871381a1c3f195
Kinsley Chinda Amadi <80432117+K1NSLEY@users.noreply.github.com>
2025-09-09 19:56:53 -0300

    Update main.rs

--- main.rs
+++ main.rs
@@ -22,23 +22,39 @@ fn main() {
     println!("-----------------------------------------------------");
     println!("              Rust Concat");
     
-    let nomes = vec!["Kinsley","Eduardo","Fisher"];
-    println!("Now {} are friends", nomes.join(", "));
+    let mut nomes = vec!["Kinsley","Eduardo","Fisher"];
+    println!("The {} are friends", nomes.join(", "));
     
     let nome1: &str = "Kinsley";
     let nome2: &str = "Eduardo";
     let nome3: &str = "Fisher";
-    println!("{}, {} e {} now are enemies",nome1,nome2,nome3);
-    
-    
-    println!();
-    println!("-----------------------------------------------------");
-    println!("                  Repeat Loops");
+    println!("Now {}, {} e {} now are enemies",nome1,nome2,nome3);
     println!("-----------------------------------------------------");
     
     
     println!();
     println!("-----------------------------------------------------");
-    println!("                  Repeat Loops");
+    println!("              Repeat Loops (for)");
     println!("-----------------------------------------------------");
+    for nome in &nomes {
+        println!("Nome: {} na lista",nome)
+    }
+    
+    println!();
+    println!("-----------------------------------------------------");
+    println!("             Interactions with batteries");
+    println!("-----------------------------------------------------");
+    //let mut nomes = vec!["Kinsley","Eduardo","Fisher"]; alredy exists
+    nomes.push("10");  // Adiciona 10 na pilha
+    let nomes = nomes.pop();  // Remove o último elemento (topo da pilha)
+    let nomes = nomes.last();  // Obtém uma referência ao topo da pilha
+    println!("A pilha está vazia? {}", nomes.is_empty());  // Verifica se a pilha está vazia
+    for nome in &nomes {
+        println!("Nome: {} na lista",nome)
+    }
+    
+    
+    
+    
+    
 }
